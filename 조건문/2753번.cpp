d9047ee7b2c7b9344fe491ff06fdec5b501429db
고태희 <127705834+koth5997@users.noreply.github.com>
2024-11-12 19:32:22 +0900

    Add files via upload

---
+++ 조건문/2753번.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+/*
+int main()
+{
+	int year;
+	cin >> year;
+
+	if (year % 4 == 0 and (year % 400 == 0 or year % 100 != 0)) //윤년일때
+	{
+		cout << "1";
+	}
+	else
+	{
+		cout << "0";
+	}
+}*/
---
+++ 조건문/9498번.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+
+/*
+int main()
+{
+	int a;
+	cin >> a; 
+
+	if (a>=90)
+	{
+		cout << "A";
+	}
+	else if (a>=80 and a<=89)
+	{
+		cout << "B";
+	}
+	else if (a>=70 and a<=79)
+	{
+		cout << "C";
+	}
+	else if (a>=60 and a<=69)
+	{
+		cout << "D";
+	}
+	else
+	{
+		cout << "F";
+	}
+	
+
+}*/
