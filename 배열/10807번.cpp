2fd8dc9ed08d49dea1f5202fdb93b7a4c52b0881
고태희 <127705834+koth5997@users.noreply.github.com>
2024-11-13 21:01:10 +0900

    Add files via upload

---
+++ 배열/10807번.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+/*
+int main()
+{
+	cin.tie(NULL);
+	ios::sync_with_stdio(false);
+	
+	int n, b=0,num=0;
+
+	cin >> n;
+	int arr[10000];
+
+	for (int i = 0; i < n; i++)
+	{
+		cin >> arr[i];
+	}
+	cin >> b;
+	for (int i = 0; i < n; i++)
+	{
+		if (arr[i] == b)
+		{
+			num = num + 1;
+			
+		}
+		
+	}
+	cout << num;
+
+	return 0;
+}*/
---
+++ 배열/10871번.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+/*
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	int n, x;
+	cin >> n >> x;
+	int a[n];
+
+	for (int i = 0; i < n; i++)
+	{
+		cin >> a[i];
+		if (a[i] < x)
+		{
+			cout << a[i] << ' ';
+		}
+	}
+
+	return 0;
+}*/
