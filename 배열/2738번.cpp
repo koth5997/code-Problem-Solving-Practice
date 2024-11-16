d599644f20c936785477f4dc19c11d88501a68ad
고태희 <127705834+koth5997@users.noreply.github.com>
2024-11-16 15:23:49 +0900

    Add files via upload

---
+++ 배열/2738번.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+/*
+int arr1[101][101], arr2[101][101];
+
+int main()
+{
+	int n, m;
+	
+
+	cin >> n >> m;
+
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < m; j++)
+			cin >> arr1[i][j];
+	}
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < m; j++)
+			cin >> arr2[i][j];
+	}
+
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < m; j++)
+		{
+			cout << arr1[i][j] + arr2[i][j] << ' ';
+		}
+		cout << "\n";
+	}
+	return 0;
+
+}*/
