e1380aa5f86309172a91dc68c0a8ff53e1c8373a
고태희 <127705834+koth5997@users.noreply.github.com>
2024-11-19 20:40:27 +0900

    Add files via upload

---
+++ 함수/15964번.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+/*
+long long gal(long long a, long long b)
+{
+	return ((a + b) * (a - b));
+}
+
+int main()
+{
+	long long a, b;
+
+	cin >> a >> b;
+	cout << gal(a, b) << endl;
+	
+	return 0;
+}*/
---
+++ 함수/2475번.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+/*
+int gum(int num[5])
+{
+	int sum = 0;
+	for (int i = 0; i < 5; i++)
+	{
+		sum += num[i] * num[i];
+	}
+	return sum % 10;
+}
+
+int main()
+{
+	int num[5];
+	for (int i = 0; i < 5; i++)
+	{
+		cin >> num[i];
+	}
+	cout << gum(num) << endl;
+
+	return 0;
+}*/
