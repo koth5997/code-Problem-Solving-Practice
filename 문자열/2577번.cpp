5b8cb1cc15a80491cd5c6c40b6876ac860547439
고태희 <127705834+koth5997@users.noreply.github.com>
2024-11-30 21:36:29 +0900

    숫자의 개수 새기

---
+++ 문자열/2577번.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <string>
+using namespace std;
+/*
+int main() {
+    int a, b, c;
+    int gap;
+    int zero = 0, one = 0, two = 0, three = 0, four = 0, five = 0, six = 0, seven = 0, eight = 0, nine = 0;
+    string gop; // gop를 여기서 선언
+
+    cin >> a >> b >> c;      
+    gap = a * b * c;         
+    gop = to_string(gap);     // 정수를 문자열로 변환
+
+    for (int i = 0; i< gop.length(); i++)
+    {
+        if (gop[i] == '0')
+        {
+            zero += 1;
+        }
+        else if (gop[i] == '1')
+        {
+            one +=1;
+        }
+        else if (gop[i] == '2')
+        {
+            two +=1;
+        }
+        else if (gop[i] == '3')
+        {
+            three +=1;
+        }
+        else if (gop[i] == '4')
+        {
+            four +=1;
+        }
+        else if (gop[i] == '5')
+        {
+            five +=1;
+        }
+        else if (gop[i] == '6')
+        {
+            six +=1;
+        }
+        else if (gop[i] == '7')
+        {
+            seven +=1;
+        }
+        else if (gop[i] == '8')
+        {
+            eight +=1;
+        }
+        else if (gop[i] == '9')
+        {
+            nine +=1;
+        }
+    }
+    cout << zero<<"\n"<< one<<"\n"<< two<< "\n"<< three << "\n" << four<< "\n"<< five<< "\n"<<six<< "\n"<< seven<< "\n"<< eight<< "\n"<<nine;
+    return 0;
+}*/
