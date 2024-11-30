ae0ed25db0fe257335d04e65c3caadbdd9b45e4e
고태희 <127705834+koth5997@users.noreply.github.com>
2024-11-30 21:37:09 +0900

    OX퀴즈

---
+++ 문자열/8958번.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include<string>
+using namespace std;
+/*
+int OX(string ran) {
+
+	int jum = 0; //+할 점수
+	int sum = 0;
+	
+	for (char v : ran)
+	{
+		if (v == 'O')
+		{
+			jum++;
+			sum += jum;
+		}
+		else {
+			jum = 0;
+
+		}
+			
+	}
+	return sum;
+
+}
+
+
+int main()
+{
+	int num;
+	cin >> num;
+
+	for (int i = 0; i < num; i++)
+	{
+		string ran;
+		cin >> ran;
+
+		cout << OX(ran) << "\n";
+	}
+
+	return 0;
+}*/
