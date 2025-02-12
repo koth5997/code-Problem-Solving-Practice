f4079d4763abd8eb709f11e930d3cdb4acbfa9d9
고태희 <127705834+koth5997@users.noreply.github.com>
2025-02-12 14:53:31 +0900

    호텔 위치계산

---
+++ 반복문/10250.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+#include<string.h>
+
+
+/*
+int main()
+{
+	int t;
+	int h, w, n; // h높이 w가로 n 사람 
+	int result;
+
+	cin >> t;
+
+	for (int i = 0; i < t; i++)
+	{
+		cin >> h >> w >> n;
+
+		if (n % h == 0) {
+			result = h * 100 + (n / h);
+		}
+		else
+		{
+			result = (n % h) * 100 + (n / h) + 1;
+		}
+		cout << result << endl;
+	}
+
+
+}*/
