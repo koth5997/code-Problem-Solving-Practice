41a7f622cc80527ce06ec64ba8b2f78abe393442
고태희 <127705834+koth5997@users.noreply.github.com>
2025-02-13 16:02:18 +0900

    Add files via upload

---
+++ 배열/2920.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include<vector>
+using namespace std;
+/*
+int main()
+{
+	vector<int> num(8);
+	for (int i = 0; i < 8; i++)
+	{
+		cin >> num[i];
+	}
+	if (num == vector<int>{1, 2, 3, 4, 5, 6, 7, 8})
+	{
+		cout << "ascending" << endl;
+	}
+	else if (num == vector<int>{8, 7, 6, 5, 4, 3, 2, 1})
+	{
+		cout << "descending" << endl;
+	}
+	else
+	{
+		cout << "mixed" << endl;
+	}
+	return 0;
+
+}*/
