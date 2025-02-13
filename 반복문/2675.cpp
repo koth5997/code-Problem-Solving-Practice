45c6015bbf785199e5e6eca36835d879949fdbd4
고태희 <127705834+koth5997@users.noreply.github.com>
2025-02-13 15:02:47 +0900

    Add files via upload

---
+++ 반복문/2675.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <sstream>
+#include <string>
+using namespace std;
+/*
+int main()
+{
+	int num;
+	
+
+	cin >> num;
+	for (int i = 0; i < num; i++)
+	{
+		int R;
+		string str;
+		cin >> R >> str;
+		
+		for (int j = 0; j < str.length(); j++)
+		{
+			for (int k = 0; k < R; k++)
+				cout << str[j];
+		}
+		cout << "\n";
+	}
+}*/
