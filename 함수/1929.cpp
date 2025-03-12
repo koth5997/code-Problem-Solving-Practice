6bb884f29b32ac7e28f6039f1934dd3096c75a3d
고태희 <127705834+koth5997@users.noreply.github.com>
2025-03-12 17:03:07 +0900

    소수구하기

---
+++ 함수/1929.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+
+#include <vector>
+using namespace std;
+
+bool isPrime(int num)
+{
+	if (num < 2)
+	{
+		return false;
+	}
+	for (int i = 2; i * i <= num; i++)
+	{
+		if (num % i == 0)
+			return false;
+	}
+	return true;
+}
+int main()
+{
+	int M, N;
+	cin >> M >> N;
+	for (int i = M; i <= N; i++)
+	{
+		if (isPrime(i))
+			cout << i << '\n';
+	}
+	return 0;
+
+
+}
